206875697836eefa0fc3a487f8350fbb22e637e5
DarioGiusino <dario.giusino@gmail.com>
2022-12-13 16:43:47 +0100

    html+js structure jsnack2

---
+++ jsnack2/script.js
@@ -0,0 +1,18 @@
+/* In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, l’utente inserisce il suo nome tramite un input.comunicagli se può partecipare o no alla festa. (vietato usare includes() ) */
+
+// definisco lista partecipanti e recupero elementi dal dom
+const partecipants = ['Dario', 'Giuseppe', 'Bartolomeo', 'Marco', 'Rosaria', 'Giulia'];
+const partecipantField = document.getElementById('partecipant');
+const verifyButton = document.getElementById('verify');
+const message = document.getElementById('result');
+
+// aggancio evento al bottone
+verifyButton.addEventListener('click', function(){
+    // prendo valore del testo inserito nell'input
+
+    // verifico sia valido
+
+    // controllo nella mia lista se esistente
+
+    // stampo su pagina messaggio appropriato a seconda dell'esito
+});
