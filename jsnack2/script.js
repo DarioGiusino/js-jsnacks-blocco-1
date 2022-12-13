2212e7052c5afc06115cae860aea4cd4eb6b57c1
DarioGiusino <dario.giusino@gmail.com>
2022-12-13 16:52:53 +0100

    added logic

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -4,15 +4,32 @@
 const partecipants = ['Dario', 'Giuseppe', 'Bartolomeo', 'Marco', 'Rosaria', 'Giulia'];
 const partecipantField = document.getElementById('partecipant');
 const verifyButton = document.getElementById('verify');
-const message = document.getElementById('result');
+const result = document.getElementById('result');
 
 // aggancio evento al bottone
 verifyButton.addEventListener('click', function(){
     // prendo valore del testo inserito nell'input
+    const userName = partecipantField.value.trim();
 
     // verifico sia valido
+    if (!userName){
+        result.innerText = 'Spiacente, hai inserito dati non validi';
+        return;
+    }
 
     // controllo nella mia lista se esistente
+    let isAuth = false;
+
+    for (i = 0; !isAuth && i < partecipants.length; i++){
+        const currentName = partecipants[i];
+
+        if (currentName === userName){
+            isAuth = true;
+        }
+    }
 
     // stampo su pagina messaggio appropriato a seconda dell'esito
+    const message = isAuth ? 'Puoi accedere alla festa' : 'Non sei stato invitato';
+
+    result.innerText = message;
 });
