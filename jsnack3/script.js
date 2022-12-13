5406b6300ee62f2e5d3462305798e54c89da3e42
DarioGiusino <dario.giusino@gmail.com>
2022-12-13 17:36:01 +0100

    commented steps

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,3 +1,19 @@
 /* L'utente inserisce un numero di 4 cifre in un input. Calcola la somma di tutte le cifre che compongono il numero e stampala in pagina.
 Esempio: l'utente inserisce 1234, noi stampiamo 10. */
 
+// prendo elementi dal dom
+const inputField = document.getElementById('numbers');
+const button = document.getElementById('button');
+const result = document.getElementById('sum');
+
+// aggancio evento al click del button
+button.addEventListener('click', function(){
+    // prendo valore dell'input
+
+    // separo il valore in singoli fattori
+
+    // sommo i fattori
+
+    // stampo su pagina il risultato
+});
+
